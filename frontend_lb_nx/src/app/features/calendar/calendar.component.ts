c7debea4a1fd0414cef234cb901ee049d27c49a1
Jumpy98 <toni.boeswetter@outlook.de>
2023-04-25 18:21:26 +0200

    Toni working on tables and entities

--- frontend_lb_nx/src/app/features/calendar/calendar.component.ts
+++ frontend_lb_nx/src/app/features/calendar/calendar.component.ts
@@ -2,21 +2,25 @@ import { Component } from '@angular/core';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import {CalendarEvent, CalendarView} from "angular-calendar";
 import {OrgEvent, Shift, ShiftType, User} from "@frontend-lb-nx/shared/entities";
+import {
+  isSameDay,
+  isSameMonth,
+} from 'date-fns';
 
 const orgEvent: OrgEvent={
   id: "asdsad", name: "Plattenbauromantik", start: new Date(Date.now()), end: new Date(Date.now()), shifts: [],
 }
 
 const shiftType:  ShiftType={
-  id: "asdassadsd", name:"Barschicht"
+  id: "asdassadsd", name:"Barschicht", value: 1
 }
 
 const shiftType2:  ShiftType={
-  id: "asdassadsdd", name:"Security"
+  id: "asdassadsdd", name:"Security", value: 1
 }
 
 const user1: User={
-  id: "2312",
+  id: "asd",
   name: "emil",
   roles: [],
   password: "xxxxx",
@@ -42,11 +46,12 @@ const user2: User={
 }
 
 const ELEMENT_DATA: Shift[] = [
-  { description: 'Hydrogen', starttime: new Date(Date.now()), endtime: new Date(Date.now()),headcount: 2, orgEvent: orgEvent, users: [], shiftType: shiftType},
-  { description: 'Helium',starttime: new Date(Date.now()), endtime: new Date(Date.now()),headcount: 2, orgEvent: orgEvent, users: [ user2], shiftType},
-  { description: 'ASDASD', starttime: new Date(Date.now()), endtime: new Date(Date.now()),headcount: 2, orgEvent: orgEvent, users: [], shiftType: shiftType2},
+  { description: 'Hydrogen', starttime: new Date(Date.now()), endtime: new Date(Date.now()),headcount: 2, orgEvent: orgEvent, users: [user1,user2], shiftType: shiftType},
+  { description: 'Helium',starttime: new Date(Date.now()), endtime: new Date(Date.now()),headcount: 2, orgEvent: orgEvent, users: [user1, user2], shiftType},
+  { description: 'ASDASD', starttime: new Date(Date.now()), endtime: new Date(Date.now()),headcount: 2, orgEvent: orgEvent, users: [user1, user2], shiftType: shiftType2},
   { description: 'SDASD',starttime: new Date(Date.now()), endtime: new Date(Date.now()),headcount: 2, orgEvent: orgEvent, users: [user1, user2], shiftType: shiftType2},
 ];
+orgEvent.shifts=ELEMENT_DATA
 
 @Component({
   selector: 'frontend-lb-nx-calendar',
@@ -55,6 +60,8 @@ const ELEMENT_DATA: Shift[] = [
 })
 export class CalendarComponent {
   view: CalendarView = CalendarView.Month;
+
+  activeDayIsOpen = true;
 
   orgEvents=[orgEvent]
 
@@ -75,8 +82,10 @@ export class CalendarComponent {
       const calEv: CalendarEvent={
         title: orgEv.name,
         start: orgEv.start,
+        actions: [],
+        meta: orgEv,
         color: {
-        primary: '#ad2121',
+        primary: '#FAE3E3',
             secondary: '#FAE3E3',
       },
       }
@@ -84,5 +93,46 @@ export class CalendarComponent {
     }
     this.events=arr
     return this.events
+  }
+
+
+  checkEventStatus(orgEvent: OrgEvent|undefined): string {
+    if(orgEvent == undefined){
+      return ""
+    }
+    //check if own user has a shift
+    const shiftUsers = orgEvent.shifts.map((s) => s.users)
+    const ownId="asd"
+    let shiftsFull = true
+    let ownHasShift= false
+    for (const shift of orgEvent.shifts) {
+      if ((shift.users?.length ?? 0) < shift.headcount) {
+        shiftsFull = false;
+      }
+      if(shift.users?.map((s)=>s.id).includes(ownId)){
+        ownHasShift=true
+      }
+    }
+    if(ownHasShift){
+      return "blue"
+    }
+    if(shiftsFull){
+      return "green"
+    }
+    return "red"
+  }
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+          (isSameDay(this.viewDate, date) && this.activeDayIsOpen) ||
+          events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
   }
 }
