7bdc384952da35c5893996fda1d906f9c3ceb2a3
Daniel Lenhardt <ckwdani@gmail.com>
2023-05-04 13:58:18 +0200

    add eddit name dialog to repo and important delete dialog

---
+++ frontend_lb_nx/src/app/core/components/dialogs/important-delete-dialog/important-delete-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'frontend-lb-nx-important-delete-dialog',
+  templateUrl: './important-delete-dialog.component.html',
+  styleUrls: ['./important-delete-dialog.component.scss'],
+})
+export class ImportantDeleteDialogComponent {}
